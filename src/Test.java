0f4f142f476e92dbc1aae056f6be00a2a086b3c3
DELL <DELL@DESKTOP-MLV1CN9>
2021-11-06 11:28:04 +0530

    First Commit

---
+++ src/Test.java
@@ -0,0 +1,19 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+    String hungry = "No";
+    
+    if (hungry == "No")
+    {
+    	System.out.println("Eat something");
+    }
+    else
+    	
+    {
+    	System.out.println("Work");
+    }
+	}
+
+}
